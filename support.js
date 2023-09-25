f069357af8905c6ad474d9a35b89783ad050c2a4
Nik <dominik.rotter.20+github@gmail.com>
2023-09-25 22:08:47 +0200

    timeout for error message in JS

--- support.js
+++ support.js
@@ -13,12 +13,16 @@ function activePage() {
     }
 }
 
-
-
 function errorMessage(identifier) {
-    if(identifier === 1){
-        document.getElementById("dialogDonate").innerHTML = "A redirect error occurred. Please try again later.";
+    let element;
+    if(identifier == 1){
+        element = document.getElementById("dialogDonate");
+        element.innerHTML = "A redirect error occurred. Please try again later.";
     }else{
-        document.getElementById("dialogContact").innerHTML = "An error occurred while submitting the form. Please try again later.";
+        element = document.getElementById("dialogContact");
+        element.innerHTML = "An error occurred while submitting the form. Please try again later.";
     }
+    setTimeout(() => {
+        element.innerHTML = "";
+    }, 5000);
 }
