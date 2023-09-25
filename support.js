9323cf022b974af12e496b56d9d760c5d8a9d60f
Nik <dominik.rotter.20+github@gmail.com>
2023-09-25 21:49:15 +0200

    error messages on buttons

--- support.js
+++ support.js
@@ -1,14 +1,24 @@
 window.addEventListener('load', activePage);
 
 function activePage() {
-    const currentLocation = location.pathname.slice(1); // remove the leading slash
-    console.log(currentLocation); // log the value of currentLocation to the console
+    const currentLocation = location.pathname.slice(1); 
+    //console.log(currentLocation); 
     const navLinks = document.querySelectorAll("nav a");
     const linkLength = navLinks.length;
 
     for (let i = 0; i < linkLength; i++) {
-        if (navLinks[i].pathname.slice(1) === currentLocation) { // remove the leading slash
+        if (navLinks[i].pathname.slice(1) === currentLocation) { 
             navLinks[i].classList.add("navActive");
         }
+    }
+}
+
+
+
+function errorMessage(identifier) {
+    if(identifier === 1){
+        document.getElementById("dialogDonate").innerHTML = "A redirect error occurred. Please try again later.";
+    }else{
+        document.getElementById("dialogContact").innerHTML = "An error occurred while submitting the form. Please try again later.";
     }
 }
